0f9727b8f13189a8b055df436f7ceb16f2efb4ef
sijilsasidharan <sijilsasidharan@gmail.com>
2024-11-20 00:14:58 +0530

    Road

--- self-drive-car/road.js
+++ self-drive-car/road.js
@@ -1,10 +1,33 @@
+/**
+ * Road class will draw road in the canvas with lanes
+ */
 class Road {
-    constructor(lanes) {
+    constructor(x, width, lanes = 3) {
+        this.x = x;
+        this.width = width;
         this.lanes = lanes;
+
+        this.left = this.x - this.width / 2;
+        this.right = this.x + this.width / 2;
+
+        const infinity = 1000000;
+        this.top = -infinity;
+        this.bottom = infinity;
     }
 
     draw(ctx) {
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = "white";
+
+        // left lane
         ctx.beginPath();
-        ctx
+        ctx.moveTo(this.left, this.top)
+        ctx.lineTo(this.left, this.bottom);
+        ctx.stroke();
+        // right lane
+        ctx.beginPath();
+        ctx.moveTo(this.right, this.top)
+        ctx.lineTo(this.right, this.bottom);
+        ctx.stroke();
     }
 }
