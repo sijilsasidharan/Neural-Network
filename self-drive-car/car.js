aeeb8a4695f1ad4b08a1b3c0793fa84c5308d110
sijilsasidharan <sijilsasidharan@gmail.com>
2024-12-03 18:38:24 +0530

    Car poly

--- self-drive-car/car.js
+++ self-drive-car/car.js
@@ -1,90 +1,93 @@
 /**
  * Car class will create a car and position it in the x and y position
  * it will define the speed, acceleration, friction, controls of the car
- * 
+ *
  */
 class Car {
-    constructor(x, y, width, height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
 
-        this.speed = 0;
-        this.acceleration = 0.2;
+    this.speed = 0;
+    this.acceleration = 0.2;
 
-        this.maxSpeed = 3;
-        this.maxReverseSpeed = 2;
-        this.friction = 0.06;
+    this.maxSpeed = 3;
+    this.maxReverseSpeed = 2;
+    this.friction = 0.06;
 
-        this.angle = 0;
+    this.angle = 0;
 
-        this.controls = new Controls();
+    this.controls = new Controls();
 
-        this.sensor = new Sensor(this);
+    this.sensor = new Sensor(this);
+  }
+
+  update(roadBorders) {
+    this.#move();
+    this.sensor.update(roadBorders);
+  }
+
+  #createPolygon() {
+    const pointes = [];
+  }
+
+  #move() {
+    if (this.controls.forward) {
+      this.speed += this.acceleration;
+    }
+    if (this.controls.reverse) {
+      this.speed -= this.acceleration;
     }
 
-    update(roadBorders) {
-        this.#move();
-        this.sensor.update(roadBorders);
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
     }
 
-    #move() {
-        if (this.controls.forward) {
-            this.speed += this.acceleration;
-        }
-        if (this.controls.reverse) {
-            this.speed -= this.acceleration;
-        }
-
-        if (this.speed > this.maxSpeed) {
-            this.speed = this.maxSpeed
-        }
-
-        if (this.speed < -this.maxReverseSpeed) {
-            this.speed = -this.maxReverseSpeed;
-        }
-
-        if (this.speed > 0) {
-            this.speed -= this.friction;
-        }
-
-        if (this.speed < 0) {
-            this.speed += this.friction;
-        }
-
-        if (Math.abs(this.speed) < this.friction) {
-            this.speed = 0;
-        }
-
-        if (this.speed !== 0) {
-            const flip = this.speed > 0 ? 1 : -1
-            if (this.controls.left) {
-                this.angle += 0.03 * flip;
-            }
-    
-            if (this.controls.right) {
-                this.angle -= 0.03 * flip;
-            }
-        }
-
-
-        this.x -= Math.sin(this.angle) * this.speed;
-        this.y -= Math.cos(this.angle) * this.speed;
+    if (this.speed < -this.maxReverseSpeed) {
+      this.speed = -this.maxReverseSpeed;
     }
 
-    draw(ctx) {
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(-this.angle);
-
-        ctx.beginPath();
-        ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
-        ctx.fillStyle ='black';
-        ctx.fill();
-
-        ctx.restore();
-        
-        this.sensor.draw(ctx);
+    if (this.speed > 0) {
+      this.speed -= this.friction;
     }
+
+    if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+
+    if (Math.abs(this.speed) < this.friction) {
+      this.speed = 0;
+    }
+
+    if (this.speed !== 0) {
+      const flip = this.speed > 0 ? 1 : -1;
+      if (this.controls.left) {
+        this.angle += 0.03 * flip;
+      }
+
+      if (this.controls.right) {
+        this.angle -= 0.03 * flip;
+      }
+    }
+
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cos(this.angle) * this.speed;
+  }
+
+  draw(ctx) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
+
+    ctx.beginPath();
+    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
+    ctx.fillStyle = "black";
+    ctx.fill();
+
+    ctx.restore();
+
+    this.sensor.draw(ctx);
+  }
 }
