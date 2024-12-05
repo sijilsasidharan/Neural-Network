b1b9bbc0de8695a1b0032bbe34e6b22187a83728
sijilsasidharan <sijilsasidharan@gmail.com>
2024-12-05 17:58:25 +0530

    Car polygon

--- self-drive-car/index.js
+++ self-drive-car/index.js
@@ -1,21 +1,21 @@
-const canvas = document.getElementById('carCanvas');
+const canvas = document.getElementById("carCanvas");
 canvas.height = window.innerHeight;
 canvas.width = 250;
 
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
 const car = new Car(road.getLaneCenter(1), 500, 50, 80);
-car.draw(ctx); 
+// car.draw(ctx);
 
-animate()
+animate();
 
 function animate() {
-    car.update(road.borders);
-    canvas.height = window.innerHeight;
-    ctx.save();
-    ctx.translate(0, -car.y + canvas.height * 0.7)
-    road.draw(ctx);
-    car.draw(ctx);
-    ctx.restore();
-    requestAnimationFrame(animate);
+  car.update(road.borders);
+  canvas.height = window.innerHeight;
+  ctx.save();
+  ctx.translate(0, -car.y + canvas.height * 0.7);
+  road.draw(ctx);
+  car.draw(ctx);
+  ctx.restore();
+  requestAnimationFrame(animate);
 }
