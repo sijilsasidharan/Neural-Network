297b3092f94b6968e387ea77e25c09f8edb6d269
sijilsasidharan <sijilsasidharan@gmail.com>
2024-12-13 23:57:45 +0530

    Newral network - Level - basis

--- self-drive-car/network.js
+++ self-drive-car/network.js
@@ -2,5 +2,6 @@ class Level {
   constructor(inputCount, outputCount) {
     this.inputs = new Array(inputCount);
     this.outputs = new Array(outputCount);
+    this.basises = new Array(outputCount);
   }
 }
