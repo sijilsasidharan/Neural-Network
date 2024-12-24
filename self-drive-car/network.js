49a6bdf96a78618d9e4b6a9afec982fc39b43b6a
sijilsasidharan <sijilsasidharan@gmail.com>
2024-12-24 23:39:13 +0530

    Randamized levels

--- self-drive-car/network.js
+++ self-drive-car/network.js
@@ -3,5 +3,21 @@ class Level {
     this.inputs = new Array(inputCount);
     this.outputs = new Array(outputCount);
     this.basises = new Array(outputCount);
+
+    this.weights = [];
+
+    for (let i = 0; i < inputCount; i++) {
+      this.weights.push(new Array(outputCount));
+    }
+
+    Level.#randomize(this.weights);
+  }
+
+  static #randomize(level) {
+    for (let i = 0; i < level.inputs.length; i++) {
+      for (let j = 0; j < level.outputs.length; j++) {
+        level.weights[i][j] = Math.random() * 2 - 1;
+      }
+    }
   }
 }
