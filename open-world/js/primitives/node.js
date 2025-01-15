4f43305ddf98cc9864cfa4c511d625a82395d73b
sijilsasidharan <sijilsasidharan@gmail.com>
2025-01-15 23:56:11 +0530

    Nodes and Edges

---
+++ open-world/js/primitives/node.js
@@ -0,0 +1,13 @@
+class Node {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  draw(ctx, size = 18, color = "black") {
+    const radius = size / 2;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, radius, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
