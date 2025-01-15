8b1c684ac95af2e048cd04022dd211c2acde0e09
sijilsasidharan <sijilsasidharan@gmail.com>
2025-01-15 23:49:20 +0530

    Graph

---
+++ open-world/js/math/Graph.js
@@ -0,0 +1,16 @@
+class Graph {
+  constructor(nodes, edges) {
+    this.nodes = nodes;
+    this.edges = edges;
+  }
+
+  draw(ctx) {
+    for (const edge of this.edges) {
+      edge.draw(ctx);
+    }
+
+    for (const node of this.nodes) {
+      node.draw(ctx);
+    }
+  }
+}
