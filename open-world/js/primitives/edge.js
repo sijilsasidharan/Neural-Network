40d1f301588fa3a868cf945e4fe5640c54458062
sijilsasidharan <sijilsasidharan@gmail.com>
2025-01-17 00:00:42 +0530

    edges

---
+++ open-world/js/primitives/edge.js
@@ -0,0 +1,13 @@
+class Edge {
+  constructor(p1, p2) {
+    this.p1 = p1;
+    this.p2 = p2;
+  }
+  draw(ctx, width = 2, color = "black") {
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.moveTo(this.p1.x, this.p1.y);
+    ctx.lineTo(this.p2.x, this.p2.y);
+    ctx.stroke();
+  }
+}
