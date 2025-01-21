34c7db39cfa83580112fb56ec6732996e8c13e96
sijilsasidharan <sijilsasidharan@gmail.com>
2025-01-21 20:00:43 +0530

    Add points by clicking mouse

--- open-world/js/GraphEditor.js
+++ open-world/js/GraphEditor.js
@@ -4,6 +4,15 @@ class GraphEditor {
     this.graph = graph;
 
     this.ctx = this.canvas.getContext("2d");
+
+    this.#addEventListeners();
+  }
+
+  #addEventListeners() {
+    this.canvas.addEventListener("mousedown", (e) => {
+      const mouse = new Node(e.offsetX, e.offsetY);
+      this.graph.addNode(mouse);
+    });
   }
 
   display() {
