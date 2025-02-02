68215d19d846c6d34b0259c21b9b9316bccdb2da
sijilsasidharan <sijilsasidharan@gmail.com>
2025-02-02 23:42:03 +0530

    viewport Center point

--- open-world/js/viewport.js
+++ open-world/js/viewport.js
@@ -6,6 +6,7 @@ class Viewport {
     this.ctx = this.canvas.getContext("2d");
 
     this.zoom = 1;
+    this.center = new Point(canvas.width / 2, canvas.height / 2);
     this.offset = new Node(0, 0);
 
     this.drag = {
