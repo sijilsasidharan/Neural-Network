3106ec8281a507c742aa12788cf968857078346f
sijilsasidharan <sijilsasidharan@gmail.com>
2025-02-13 17:14:08 +0530

    Polygon to represent roaad

---
+++ open-world/js/primitives/polygon.js
@@ -0,0 +1,22 @@
+class Polygon {
+  constructor(nodes) {
+    this.nodes = nodes;
+  }
+
+  draw(
+    ctx,
+    { stroke = "blue", lineWidth = 2, fill = "rgba(0, 0, 255, 0.1)" } = {}
+  ) {
+    ctx.beginPath();
+    ctx.fillStyle = fill;
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = stroke;
+    ctx.moveTo(this.nodes[0].x, this.nodes[0].y);
+    for (let i = 1; i < this.nodes.length; i++) {
+      ctx.lineTo(this.nodes[i].x, this.nodes[i].y);
+    }
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+  }
+}
