329fbed21294768cfd48d332749a3e82d0cc8e64
sijilsasidharan <sijilsasidharan@gmail.com>
2025-02-16 12:39:37 +0530

    Envelop fixes

--- open-world/js/primitives/envelop.js
+++ open-world/js/primitives/envelop.js
@@ -15,12 +15,19 @@ class Envelop {
     // clock wise
     const alpha_cw = alpha + Math.PI / 2;
     // counter clock wise
-    const alpha_ccw = alpha + Math.PI / 2;
+    const alpha_ccw = alpha - Math.PI / 2;
 
     const p1_ccw = translate(p1, alpha_ccw, radius);
     const p2_ccw = translate(p2, alpha_ccw, radius);
     const p2_cw = translate(p2, alpha_cw, radius);
     const p1_cw = translate(p1, alpha_cw, radius);
+
+    const points = [];
+    const step = Math.PI / 3;
+
+    for (let i = alpha_ccw; i <= alpha_cw; i += step) {
+      points.push(translate(p1, i, radius));
+    }
 
     return new Polygon([p1_ccw, p2_ccw, p2_cw, p1_cw]);
   }
