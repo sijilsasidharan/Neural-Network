b884d88841dfe17db0f61ae093e761b829dc1869
sijilsasidharan <sijilsasidharan@gmail.com>
2025-02-16 12:47:35 +0530

    Envelop roundness update

--- open-world/js/primitives/envelop.js
+++ open-world/js/primitives/envelop.js
@@ -1,15 +1,15 @@
 class Envelop {
-  constructor(skeleton, width) {
+  constructor(skeleton, width, roundness = 10) {
     this.skeleton = skeleton;
     this.width = width;
 
-    this.poly = this.#createPoly();
+    this.poly = this.#createPoly(width, roundness);
   }
 
-  #createPoly() {
+  #createPoly(width, roundness) {
     const { p1, p2 } = this.skeleton;
 
-    const radius = this.width / 2;
+    const radius = width / 2;
     const alpha = angle(subtract(p1, p2));
 
     // clock wise
@@ -23,7 +23,7 @@ class Envelop {
     const p1_cw = translate(p1, alpha_cw, radius);
 
     const points = [];
-    const step = Math.PI / 10;
+    const step = Math.PI / Math.max(1, roundness);
 
     for (let i = alpha_ccw; i <= alpha_cw; i += step) {
       points.push(translate(p1, i, radius));
