d568e7b66346fecb977357d977b36d3314257a63
sijilsasidharan <sijilsasidharan@gmail.com>
2025-02-17 18:33:50 +0530

    World initial

--- open-world/js/world.js
+++ open-world/js/world.js
@@ -1,3 +1,12 @@
 class World {
-  constructor() {}
+  constructor(graph, roadWidth = 100, roadRoundness = 3) {
+    this.graph = graph;
+    this.roadWidth = roadWidth;
+    this.roadRoundness = roadRoundness;
+
+    this.envolop = [];
+    this.generate();
+  }
+
+  generate() {}
 }
