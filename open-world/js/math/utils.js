35ec045c77a0fda6a8661a1dae00608eeb63a305
sijilsasidharan <sijilsasidharan@gmail.com>
2025-02-23 19:40:26 +0530

    Utils update

--- open-world/js/math/utils.js
+++ open-world/js/math/utils.js
@@ -35,3 +35,25 @@ function translate(loc, angle, offset) {
 function angle(p) {
   return Math.atan2(p.y, p.x);
 }
+
+const LERP = (A, B, t) => A + (B - A) * t;
+
+function getIntersection(A, B, C, D) {
+  const tTop = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
+  const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
+  const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
+
+  if (bottom != 0) {
+    const t = tTop / bottom;
+    const u = uTop / bottom;
+    if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+      return {
+        x: LERP(A.x, B.x, t),
+        y: LERP(A.y, B.y, t),
+        offset: t,
+      };
+    }
+  }
+
+  return null;
+}
